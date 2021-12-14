f070d4ac9347cc0b5820297bbdc0b92c783e2025
ILOWSKI Piotr <ilowski.piotr@europe.essilor.group>
2021-12-14 14:35:03 +0100

    Add PL commentary to admin.py

--- Calendar/Cal/admin.py
+++ Calendar/Cal/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from .models import Event, EventMember
 
 # Register your models here.
+# PL - Zarejestruj swoj model w tym miejscu
 
 admin.site.register(Event)
 admin.site.register(EventMember)
